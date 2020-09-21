11c9af8098692434d48763a226754d582988122a
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-09-21 17:23:29 +0900

    일단은 초안

--- src/App.js
+++ src/App.js
@@ -1,26 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const movies = [
+  {
+    id: 1,
+    title: "기생충",
+    year: 2019,
+    poster:
+      "https://movie-phinf.pstatic.net/20190528_36/1559024198386YVTEw_JPEG/movie_image.jpg",
+  },
+  {
+    id: 2,
+    title: "명량",
+    year: 2014,
+    poster:
+      "https://movie-phinf.pstatic.net/20140721_186/1405911310756Tt2X1_JPEG/movie_image.jpg",
+  },
+  {
+    id: 3,
+    title: "테넷",
+    year: 2020,
+    poster:
+      "https://movie-phinf.pstatic.net/20200728_86/15959209130236RePy_JPEG/movie_image.jpg",
+  },
+  {
+    id: 4,
+    title: "강철비",
+    year: 2017,
+    poster:
+      "https://movie-phinf.pstatic.net/20171129_216/1511931096509Q0hpu_JPEG/movie_image.jpg",
+  },
+  {
+    id: 5,
+    title: "변호인",
+    year: 2013,
+    poster:
+      "https://movie-phinf.pstatic.net/20131203_145/1386034788519v2Vwy_JPEG/movie_image.jpg",
+  },
+  {
+    id: 6,
+    title: "국제시장",
+    year: 2014,
+    poster:
+      "https://movie-phinf.pstatic.net/20141124_107/141679124450580TTS_JPEG/movie_image.jpg",
+  },
+  {
+    id: 7,
+    title: "베테랑",
+    year: 2015,
+    poster:
+      "https://movie-phinf.pstatic.net/20150622_131/14349365467550iQnC_JPEG/movie_image.jpg",
+  },
+  {
+    id: 8,
+    title: "도둑들",
+    year: 2012,
+    poster:
+      "https://movie-phinf.pstatic.net/20120718_209/1342589585791cltsr_JPEG/movie_image.jpg",
+  },
+  {
+    id: 9,
+    title: "전우치",
+    year: 2009,
+    poster:
+      "https://movie-phinf.pstatic.net/20111223_249/1324598282916rToPJ_JPEG/movie_image.jpg",
+  },
+  {
+    id: 10,
+    title: "검사외전",
+    year: 2016,
+    poster:
+      "https://movie-phinf.pstatic.net/20160112_146/1452573577623EyUOh_JPEG/movie_image.jpg",
+  },
+];
+
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+const items = shuffle(movies);
+
+const App = () => {
+  let count = 0;
+  const [value, setValue] = useState("");
+  const [movie, setMovie] = useState(items[count]);
+  const [year, setYear] = useState(movie.year);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">{movie.title}</h1>
+      <img
+        className="movie__poster"
+        src={movie.poster}
+        alt={movie.title}
+        title={movie.title}
+      />
+      <form>
+        <input
+          className="movie__year__input"
+          type="text"
+          value={value}
+          placeholder="개봉년도를 입력하시오."
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+        <input
+          className="movie__year__button"
+          type="submit"
+          value="확인"
+          onClick={(e) => {
+            e.preventDefault();
+            if (Number(value) === year) {
+              console.log("correct!");
+              count += 1;
+              setMovie(items[count]);
+              setYear(movie.year);
+              setValue("");
+            } else {
+              console.log("no");
+            }
+          }}
+        />
+      </form>
     </div>
   );
-}
+};
 
 export default App;
