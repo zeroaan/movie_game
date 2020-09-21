bb1bb09ad518ef5924fbf65e02e88768dfa3851e
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-09-21 22:56:30 +0900

    Updates

---
+++ precache-manifest.2e999402a2063754b0be3353a3addeae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ceea1f7ef3d95ebf26e393b48dcc52c",
+    "url": "/movie_game/index.html"
+  },
+  {
+    "revision": "ccc19ce7d5123cda3ced",
+    "url": "/movie_game/static/css/main.61a84a0e.chunk.css"
+  },
+  {
+    "revision": "dc67f471b09a8ea3044d",
+    "url": "/movie_game/static/js/2.04c1b560.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_game/static/js/2.04c1b560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccc19ce7d5123cda3ced",
+    "url": "/movie_game/static/js/main.51539fd3.chunk.js"
+  },
+  {
+    "revision": "4d3e70133b2bb76e4ac8",
+    "url": "/movie_game/static/js/runtime-main.965733c1.js"
+  }
+]);
