7dc02fe977da866e2b741a9101b928f74bd3ff4d
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2020-09-22 16:53:43 +0900

    틀렸을 때 다시하기 기능 만들었음

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -17,6 +17,73 @@ let highscore = 0;
 const Movies = () => {
   const [value, setValue] = useState("");
   const [movie, setMovie] = useState(items[count]);
+  const [incorrect, setIncorrect] = useState(false);
+
+  let mode = null;
+  if (incorrect === false) {
+    mode = (
+      <div>
+        <h1 className="title">{movie.title}</h1>
+        <img
+          className="movie__poster"
+          src={movie.poster}
+          alt={movie.title}
+          title={movie.title}
+        />
+        <form className="movie__form">
+          <input
+            className="movie__year__input"
+            type="text"
+            value={value}
+            placeholder="개봉년도 입력 (숫자만 입력)"
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+          />
+          <input
+            className="movie__year__button"
+            type="submit"
+            value="확인"
+            onClick={(e) => {
+              e.preventDefault();
+              if (Number(value) === movie.year) {
+                count += 1;
+                if (highscore < count) {
+                  highscore = count;
+                }
+                setMovie(items[count]);
+                setValue("");
+              } else {
+                items = shuffle(movies);
+                count = 0;
+                setMovie(items[count]);
+                setValue("");
+                setIncorrect(true);
+              }
+            }}
+          />
+        </form>
+        <h2 className="correct_count">
+          맞춘 갯수: <b>{count}</b>
+        </h2>
+      </div>
+    );
+  } else {
+    mode = (
+      <div className="movie__incorrect">
+        <h1 className="movie__wrong">개봉년도가 맞지 않습니다</h1>
+        <h2 className="movie__wrongre">다시하려면 아래 버튼을 누르세요</h2>
+        <input
+          type="button"
+          className="movie__re"
+          value="다시하기"
+          onClick={() => {
+            setIncorrect(false);
+          }}
+        ></input>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
@@ -34,50 +101,9 @@ const Movies = () => {
           opacity: "0.4",
         }}
       ></div>
-      <h1 className="title">{movie.title}</h1>
-      <img
-        className="movie__poster"
-        src={movie.poster}
-        alt={movie.title}
-        title={movie.title}
-      />
       <h4 className="movie__highscore">최고 점수: {highscore}</h4>
       <h4 className="movie__currentscore">현재 점수: {count}</h4>
-      <form className="movie__form">
-        <input
-          className="movie__year__input"
-          type="text"
-          value={value}
-          placeholder="개봉년도 입력 (숫자만 입력)"
-          onChange={(e) => {
-            setValue(e.target.value);
-          }}
-        />
-        <input
-          className="movie__year__button"
-          type="submit"
-          value="확인"
-          onClick={(e) => {
-            e.preventDefault();
-            if (Number(value) === movie.year) {
-              count += 1;
-              if (highscore < count) {
-                highscore = count;
-              }
-              setMovie(items[count]);
-              setValue("");
-            } else {
-              items = shuffle(movies);
-              count = 0;
-              setMovie(items[count]);
-              setValue("");
-            }
-          }}
-        />
-      </form>
-      <h2 className="correct_count">
-        맞춘 갯수: <b>{count}</b>
-      </h2>
+      {mode}
     </div>
   );
 };
