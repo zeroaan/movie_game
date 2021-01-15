64ba8aa07d9a082a1baf39df80c1f529de861098
Jinyoung Ahn <48481448+zeroaan@users.noreply.github.com>
2021-01-15 21:33:27 +0900

    Update: movie

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,29 +1,42 @@
 import React, { useState } from "react"
 import { useSelector } from "react-redux"
+import styled from "styled-components"
 
 import MovieGame from "components/MovieGame"
 import MovieRestart from "components/MovieRestart"
 
 import "./Movies.css"
 
+const H1HighScore = styled.h1`
+  position: absolute;
+  top: 15px;
+  left: 20px;
+  font-size: 25px;
+  @media screen and (max-width: 600px) {
+    font-size: 18px;
+  }
+`
+const H1CurrentScore = styled.h1`
+  position: absolute;
+  top: 15px;
+  right: 20px;
+  font-size: 25px;
+  @media screen and (max-width: 600px) {
+    font-size: 18px;
+  }
+`
+
 const Movies = () => {
   const { score, bestScore } = useSelector((state) => state.movie)
 
-  const [incorrect, setIncorrect] = useState(true)
-
-  let mode = null
-  if (incorrect === false) {
-    mode = <MovieGame setIncorrect={setIncorrect} />
-  } else {
-    mode = <MovieRestart setIncorrect={setIncorrect} />
-  }
+  const [incorrect, setIncorrect] = useState(false)
 
   return (
-    <div className="App">
-      <h4 className="movie__highscore">최고 점수: {bestScore}</h4>
-      <h4 className="movie__currentscore">현재 점수: {score}</h4>
-      {mode}
-    </div>
+    <>
+      <H1HighScore>최고 점수: {bestScore}</H1HighScore>
+      <H1CurrentScore>현재 점수: {score}</H1CurrentScore>
+      {incorrect ? <MovieRestart setIncorrect={setIncorrect} /> : <MovieGame setIncorrect={setIncorrect} />}
+    </>
   )
 }
 
